6483c9a40f8d1a29977fe657d795ef6dc6bf10d0
slava <slava@satellogic.com>
2017-06-24 02:14:12 +0300

    access

--- dictools/dic.py
+++ dictools/dic.py
@@ -1,3 +1,7 @@
+class DicError(Exception):
+    pass
+
+
 class Dic:
     """ pairs of words in 2 languages. """
     def __init__(self, lng_src=None, lng_dst=None, dic=None):
@@ -15,3 +19,17 @@ class Dic:
 
     def __eq__(self, other):
         return self.lng_src == other.lng_src and self.lng_dst == other.lng_dst and self.dic == other.dic
+
+    def __getitem__(self, item):
+        if not isinstance(item, int) or item < 0 or item >= self.__len__():
+            raise DicError('expected valid index, got %s' % item)
+        return self.dic[item]
+
+    def __setitem__(self, key, value):
+        if not isinstance(key, int) or key < 0 or key >= self.__len__():
+            raise DicError('expected valid index, got %s' % key)
+
+        if len(value) != 2:
+            raise DicError('expected list of 2, got %s' % value)
+
+        self.dic[key] = value
--- tests/test_dic.py
+++ tests/test_dic.py
@@ -1,11 +1,12 @@
 import unittest
 
-from dictools.dic import Dic
+from dictools.dic import Dic, DicError
 
 
 class TestDic(unittest.TestCase):
 
-    sample = Dic('fr', 'en', [('moi', 'I'), ('toi', 'you')])
+    def setUp(self):
+        self.sample = Dic('fr', 'en', [('moi', 'I'), ('toi', 'you')])
 
     def test_len(self):
         self.assertEqual(len(self.sample), 2)
@@ -15,3 +16,14 @@ class TestDic(unittest.TestCase):
         self.assertNotEqual(self.sample, Dic('fa', 'en', [('moi', 'I'), ('toi', 'you')]))
         self.assertNotEqual(self.sample, Dic('fr', 'es', [('moi', 'I'), ('toi', 'you')]))
         self.assertNotEqual(self.sample, Dic('fr', 'en', [('moI', 'I'), ('toi', 'you')]))
+
+    def test_access(self):
+        self.sample[1] = ['il', 'he']
+        self.assertEqual(self.sample[1], ['il', 'he'])
+
+        for invalid in [-1, 2, 's']:
+            with self.assertRaises(DicError):
+                print(self.sample[invalid])
+            with self.assertRaises(DicError):
+                self.sample[invalid] = ('moi', 'I')
+
